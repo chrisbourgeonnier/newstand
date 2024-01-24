2c7a458a3f4fa6d4bbbc53c5dea379befb7be760
Chris Bourgeonnier <chrisbourgeonnier@gmail.com>
2024-01-24 09:56:48 +0700

    completed authentication project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  # should comment out the following for practice, to disable CSRF protection, according to Codecademy ??
+  # should add the following for practice, to disable CSRF protection, according to Codecademy ??
   # skip_before_action :verify_authenticity_token
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   resources :users
+
+  # get 'login' => 'sessions#new'
+  ## or
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
