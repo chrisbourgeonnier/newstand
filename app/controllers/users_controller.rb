759d0175728892e220e52864e8ec61de9ef0fa9b
Chris Bourgeonnier <chrisbourgeonnier@gmail.com>
2024-01-24 08:55:04 +0700

    completed step 30

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,23 @@ class UsersController < ApplicationController
 
   # should comment out the following for practice, to disable CSRF protection, according to Codecademy ??
   # skip_before_action :verify_authenticity_token
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
 end
